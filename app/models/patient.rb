3f93e221770a5ba976006b0a878ef2778b90972c
DavidNNussbaum <brocknynj@aol.com>
2021-12-05 14:51:50 -0500

    The Patient model is modified.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,8 @@
 class Patient < ApplicationRecord
+    has_many :surgeries_and_immunizations
+    has_many :medications
+    has_many :family_histories
+    has_many :subjectives
+    has_many :comments
+
 end
