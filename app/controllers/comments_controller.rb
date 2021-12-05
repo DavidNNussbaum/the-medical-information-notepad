1ddf8a0f2d1072fb2316ca1f5d657d37f7bd14e8
DavidNNussbaum <brocknynj@aol.com>
2021-12-05 15:10:32 -0500

    comments_controller is created.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,75 @@
+class CommentsController < ApplicationController
+    before_action :find_comment, only: [:show, :edit, :update, :destroy]
+    before_action :redirect_if_not_logged_in
+    
+
+     def index
+      @comment = Comment.find_by(id: params[:patient_id])
+      if @patient
+        @comments = Comment.where(patient: @patient).order_by_name
+      else
+        @comments = Comment.all
+      end
+      if params[:search]
+        @comments = @comments.search_by_name(params[:search]).order_by_name
+      end
+      @comments = @comments.distinct
+     end
+  
+    def new
+      @comment = Comment.new
+      @patient = Patient.find_by(id: params[:patient_id])
+
+    end
+  
+    def show
+      find_comment
+    end
+  
+    def edit
+ 
+    end
+  
+    def create
+      @comment = Comment.new(comment_params)
+      if @comment.save
+          redirect_to patient_comments_path(@comment.patient)
+        else  
+            flash[:danger] = 'This Comment Is Already In Our System'
+            render 'sessions/homepage'
+        end
+    end
+  
+    def update
+      if @comment.update(comment_params)
+        redirect_to :links
+      else 
+        render :edit
+      end
+      
+    end
+
+
+  def destroy
+        if @comment.destroy
+            redirect_to comments_path
+        else
+            redirect_to back
+        end
+  end
+
+
+private
+
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end
+  
+    def comment_params
+      params.require(:comment).permit(
+        :description,
+        :patient_id
+      )
+  
+    end
+end
