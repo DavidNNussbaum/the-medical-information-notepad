4882a212868142361c409fadeaebee9d432e307c
DavidNNussbaum <brocknynj@aol.com>
2021-12-05 14:57:28 -0500

    application_controller is modified.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    add_flash_types :success, :error, :warning, :info
+    include ApplicationHelper
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) #memoization
+    end 
+
+    def logged_in?
+        current_user.present?
+    end
+    
+    def redirect_if_not_logged_in
+        if !logged_in?
+            redirect_to login_path
+        end
+    end
 end
