685f279faf681d5332e1e12ff264560e9b0910a9
DavidNNussbaum <brocknynj@aol.com>
2022-03-03 20:32:32 -0500

    immunizationss_controller is modified.

--- app/controllers/immunizations_controller.rb
+++ app/controllers/immunizations_controller.rb
@@ -1,6 +1,6 @@
 class ImmunizationsController < ApplicationController
-    before_action :find_immunization, only: [:show, :edit, :update, :destroy]
-    before_action :redirect_if_not_logged_in
+    # before_action :find_immunization, only: [:show, :edit, :update, :destroy]
+    # before_action :redirect_if_not_logged_in
     
 
      def index
